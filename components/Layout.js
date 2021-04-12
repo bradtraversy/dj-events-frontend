4af25c243d262445a443321bd61526b7f5676583
Brad Traversy <traversymedia@gmail.com>
2021-04-12 11:36:07 -0400

    Showcase component

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import Header from './Header'
 import Footer from './Footer'
+import Showcase from './Showcase'
 import styles from '@/styles/Layout.module.css'
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter()
+
   return (
     <div>
       <Head>
@@ -13,6 +17,9 @@ export default function Layout({ title, keywords, description, children }) {
       </Head>
 
       <Header />
+
+      {router.pathname === '/' && <Showcase />}
+
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css'
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome To The Party!</h1>
+      <h2>Find the hottest DJ events</h2>
+    </div>
+  )
+}
