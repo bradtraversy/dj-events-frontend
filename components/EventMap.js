3b2e550e6c744a51397a715a7db42b0b72be9f0f
Brad Traversy <traversymedia@gmail.com>
2021-04-14 11:46:00 -0400

    Add map to event page

---
+++ components/EventMap.js
@@ -0,0 +1,50 @@
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import ReactMapGl, { Marker } from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+import Geocode from 'react-geocode'
+
+export default function EventMap({ evt }) {
+  const [lat, setLat] = useState(null)
+  const [lng, setLng] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [viewport, setViewport] = useState({
+    latitude: 40.712772,
+    longitude: -73.935242,
+    width: '100%',
+    height: '500px',
+    zoom: 12,
+  })
+
+  useEffect(() => {
+    // Get latitude & longitude from address.
+    Geocode.fromAddress(evt.address).then(
+      (response) => {
+        const { lat, lng } = response.results[0].geometry.location
+        setLat(lat)
+        setLng(lng)
+        setViewport({ ...viewport, latitude: lat, longitude: lng })
+        setLoading(false)
+      },
+      (error) => {
+        console.error(error)
+      }
+    )
+  }, [])
+
+  Geocode.setApiKey(process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY)
+
+  if (loading) return false
+
+  return (
+    <ReactMapGl
+      {...viewport}
+      mapboxApiAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
+      onViewportChange={(vp) => setViewport(vp)}
+    >
+      <Marker key={evt.id} latitude={lat} longitude={lng}>
+        <Image src='/images/pin.svg' width={30} height={30} />
+      </Marker>
+    </ReactMapGl>
+  )
+}
--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -4,6 +4,7 @@ import { FaPencilAlt, FaTimes } from 'react-icons/fa'
 import Link from 'next/link'
 import Image from 'next/image'
 import Layout from '@/components/Layout'
+import EventMap from '@/components/EventMap'
 import { API_URL } from '@/config/index'
 import styles from '@/styles/Event.module.css'
 import { useRouter } from 'next/router'
@@ -35,6 +36,8 @@ export default function EventPage({ evt }) {
         <p>{evt.description}</p>
         <h3>Venue: {evt.venue}</h3>
         <p>{evt.address}</p>
+
+        <EventMap evt={evt} />
 
         <Link href='/events'>
           <a className={styles.back}>{'<'} Go Back</a>
