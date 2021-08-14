51ddff44bbc200e64757f85ba55560ac5f8952a7
RoskiDeluge <roberto.delgado@gmail.com>
2021-08-13 22:25:45 -1000

    Reverting to sfbf

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import styles from "@/styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <p>Copyright &copy; RDMS 2021</p>
+      <p>Copyright &copy; RDMS General Site 2021</p>
       <p>
         <Link href="/about">About</Link>
       </p>
